8e39140b59422075f43a9d0936b9fdae639cf1b5
Mhd Ulil Abshar <87646428+uldevjs@users.noreply.github.com>
2024-02-02 23:44:26 +0700

    change style home page

--- src/component/hero/Frame.jsx
+++ src/component/hero/Frame.jsx
@@ -1,12 +1,15 @@
-import Aul from "/src/assets/howl.jpg"
+import Aul from "/src/assets/howl.jpg";
 
-const Frame = ()=>{
-    return(
-        <>
-         <img className="rounded-full  h-40 border-amber-300 border-2  " src={Aul} alt="" />  
-        </>
-    )
+const Frame = () => {
+  return (
+    <>
+      <img
+        className="rounded-full  h-40 border-gray-100 border-2  "
+        src={Aul}
+        alt=""
+      />
+    </>
+  );
+};
 
-}
-
-export default Frame
+export default Frame;
--- src/component/navbar/NavList.jsx
+++ src/component/navbar/NavList.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 const NavList = () => {
   return (
-    <nav className="border rounded-full px-3 border-yellow-400 ">
+    <nav className="border rounded-full px-3 border-gray-300">
       <ul className="flex justify-between align-middle items-center ">
         <li className="text-gray-300 px-3 py-2 ">
           <Link
             to="/About"
-            className="nav-item  text-sm font-semibold hover:text-yellow-400 transition-all duration-500 ease-in-out"
+            className="nav-item  text-sm font-semibold hover:text-lime-400 transition-all duration-500 ease-in-out"
           >
             About
           </Link>
@@ -15,7 +15,7 @@ const NavList = () => {
         <li className="text-gray-300 px-3 py-2">
           <Link
             to="/Project"
-            className="nav-item text-sm font-semibold hover:text-yellow-400 transition-all duration-500 ease-in-out "
+            className="nav-item text-sm font-semibold hover:text-lime-400 transition-all duration-500 ease-in-out "
           >
             Project
           </Link>
@@ -23,7 +23,7 @@ const NavList = () => {
         <li className="text-gray-300 px-3 py-2">
           <Link
             to="/Experience"
-            className="nav-item   text-sm font-semibold hover:text-yellow-400 transition-all duration-500 ease-in-out"
+            className="nav-item   text-sm font-semibold hover:text-lime-400 transition-all duration-500 ease-in-out"
           >
             Experience
           </Link>
@@ -31,7 +31,7 @@ const NavList = () => {
         <li className="text-gray-300 px-3 py-2">
           <Link
             to="/Contact"
-            className="nav-item   text-sm font-semibold hover:text-yellow-400 transition-all duration-500 ease-in-out"
+            className="nav-item   text-sm font-semibold hover:text-lime-400 transition-all duration-500 ease-in-out"
           >
             Contact
           </Link>
