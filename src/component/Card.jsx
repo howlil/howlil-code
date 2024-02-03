35a63f2d2ae8a860f09a454107eceb76fb48a40b
Mhd Ulil Abshar <87646428+uldevjs@users.noreply.github.com>
2024-02-03 17:27:29 +0700

    responsive card

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -9,9 +9,9 @@ import Project from "../pages/Project";
 const Card = () => {
 
   return (
-   <div className=" flex flex-wrap justify-start  ">
+   <div className="  flex flex-wrap justify-start  ">
     {dataProject.map((Project,index)=>(
-      <div key={index} className=" w-1/3 p-4 mt-3  hover:shadow-sm hover:bg-neutral-800 rounded-lg transition-all ease-in-out duration-500 ">
+      <div key={index} className="max-sm:w-full  md:w-1/3 p-4 mt-3  hover:shadow-sm hover:bg-neutral-800 rounded-lg transition-all ease-in-out duration-500 ">
         <div className="flex justify-between  items-center">
           <h1 className="text-gray-50 font-semibold text-xl">{Project.title}</h1>
           {Project.github && (
